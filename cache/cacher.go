c6ba0151a6c7c272e56a05054e9a1d4c273546bb
Ajay Dhyani <adhyani@singlestore.com>
2024-11-01 11:41:58 +0530

    chore(cache): improve documentation

--- cache/cacher.go
+++ cache/cacher.go
@@ -2,9 +2,20 @@ package cache
 
 import "time"
 
-// Cacher is an interface for a cache
+// Cacher is an interface that defines methods for a cache system.
+// It includes methods to put data into the cache, check for the existence
+// of data, and retrieve data from the cache.
 type Cacher interface {
-	Put([]byte, []byte, time.Duration) error
-	Has([]byte) bool
-	Get([]byte) ([]byte, error)
+	// Put stores the given value in the cache with the specified key and
+	// expiration duration. It returns an error if the operation fails.
+	Put(key []byte, value []byte, duration time.Duration) error
+
+	// Has checks if the given key exists in the cache. It returns true if
+	// the key is found, otherwise false.
+	Has(key []byte) bool
+
+	// Get retrieves the value associated with the given key from the cache.
+	// It returns the value and an error if the operation fails or the key
+	// does not exist.
+	Get(key []byte) ([]byte, error)
 }
